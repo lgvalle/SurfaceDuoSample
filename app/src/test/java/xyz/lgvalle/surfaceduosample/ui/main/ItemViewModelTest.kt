54120153d1ed445cf42e0e4003c322c7d4bcd1fc
lgvalle <luis.gvalle@gmail.com>
2020-04-17 15:44:26 +0200

    Add a failing test

---
+++ app/src/test/java/xyz/lgvalle/surfaceduosample/ui/main/ItemViewModelTest.kt
@@ -0,0 +1,11 @@
+package xyz.lgvalle.surfaceduosample.ui.main
+
+import org.junit.jupiter.api.Test
+
+class ItemViewModelTest {
+
+    @Test
+    internal fun `a failing test`() {
+        assert(false)
+    }
+}
